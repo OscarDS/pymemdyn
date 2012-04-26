ea2032af991ad38368d499f1ca12f49d8d155c09
Xabi Bello <xbello@gmail.com>
2012-04-26 11:56:01 +0200

    Added settings file

---
+++ settings.py
@@ -0,0 +1,8 @@
+#import pymoldyn
+
+#SCRIPT_DIR = os.path.dirname(os.path.realpath(pymoldyn.__file__))
+#ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+REPO_DIR = "templates"
+
+QUEUE = "slurm"
