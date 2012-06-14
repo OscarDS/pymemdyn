eea11a72905de86c2595ed17b99a48f0eb8f01ec
Xabi Bello <xbello@gmail.com>
2012-06-14 10:56:26 +0200

    Added pds and pbs_ib to queue.py

--- queue.py
+++ queue.py
@@ -5,7 +5,7 @@ class Queue(object):
     def __init__(self, *args, **kwargs):
         self.num_proc = 8 #Default number of processors to be used
         self.max_time = "50:00:00"
-        self.sh = "mdrun.sh"
+        self.sh = "./mdrun.sh"
 
     def set_mdrun(self, value):
         '''Sets the md_run command'''
@@ -61,28 +61,69 @@ class PBS(Queue):
     def __init__(self, *args, **kwargs):
         super(PBS, self).__init__(self, *args, **kwargs)
         '''Setting the command to run mdrun in pbs queue with mpi'''
-        self.num_proc = 40
-        self.max_time = "08:00:00"
-        self.max_cpu_time = "320:00:00"
-        self.max_mem = "12gb"
-        self.sh = "mdrun.sh"
+        # These values are here for reference, doesn't do NOTHING      #
+        # Calling file run.sh should resemble this lines               #
+        self.num_nodes = 5                                             #
+        self.proc_per_node = 8                                         #
+        self.max_time = "36:00:00"                                     #
+        self.max_cpu_time = "1440:00:00"                               #
+        self.max_mem = "12gb"                                          #
+        self.command = ["qsub",                                        #
+            "-nodes=%d:ppn=%d" % (self.num_nodes, self.proc_per_node), #
+            "-walltime=%s" % self.max_time,                            #
+            "-cput=%s" % self.max_cpu_time,                            #
+            "-mem=%s" % self.max_mem,                                  #
+                                                                       #
+        ################################################################
 
-        self._mdrun="mdrun_mpi"
+        self._mdrun=os.path.join(settings.GROMACS_PATH, "mdrun_mpi")
+        self.command = [self.sh]
 
-        self.command = ["qsub",
-            "-nodes=%d:ppn=4:ib" % self.num_proc,
-            "-walltime=%s" % self.max_time,
-            "-cput=%s" % self.max_cpu_time,
-            "-mem=%s" % self.max_mem,
-            self.sh]
     def make_script(self, workdir, options):
         '''PBS must load some modules in each node by shell scripts
         options is a list with all the options'''
         sh = open(self.sh, "w")
         sh.write("#!/bin/bash\n")
-        sh.write("cd $PBS_O_WORKDIR\n")
+        sh.write("cd %s\n" % os.path.join(os.getcwd(), workdir))
+        sh.write("module load gromacs/4.0.5-gige\n")
+        sh.write("mpirun %s %s -v &>mdrun.log\n" \
+            % (self.mdrun, " ".join(options)))
+        sh.close()
+        os.chmod(self.sh, 0755)
+
+        return True
+
+class PBS_IB(Queue):
+    def __init__(self, *args, **kwargs):
+        super(PBS, self).__init__(self, *args, **kwargs)
+        # USELESS, see class PBS for explanation                            #
+        self.num_nodes = 10                                                 #
+        self.proc_per_node = 4                                              #
+        self.max_time = "08:00:00"                                          #
+        self.max_cpu_time = "320:00:00"                                     #
+        self.max_mem = "12gb"                                               #
+                                                                            #
+        self.command = [                                                    #
+            "qsub",                                                         #
+            "-l nodes=%d:ppn=%d:ib" % (self.num_nodes, self.proc_per_node), #
+            "-l walltime=%s" % self.max_time,                               #
+            "-l cput=%s" % self.max_cpu_time,                               #
+            "-l mem=%s" % self.max_mem,                                     #
+            self.sh]                                                        #
+        #####################################################################
+
+        self._mdrun=os.path.join(settings.GROMACS_PATH, "mdrun_mpi")
+        self.command = [self.sh]
+
+    def make_script(self, workdir, options):
+        '''PBS must load some modules in each node by shell scripts
+        options is a list with all the options'''
+        sh = open(self.sh, "w")
+        sh.write("#!/bin/bash\n")
+        sh.write("cd %s\n" % os.path.join(os.getcwd(), workdir))
         sh.write("module load gromacs\n")
-        sh.write("mpirun %s %s\n" % (self.mdrun, " ".join(options)))
+        sh.write("mpirun %s %s -v &>mdrun.log\n" \
+            % (self.mdrun, " ".join(options)))
         sh.close()
         os.chmod(self.sh, 0755)
 
