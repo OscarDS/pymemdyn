eb1e178cc2fce9f92693fa8456fbc8f212b30c76
Mauricio Esguerra <mauricio.esguerra@gmail.com>
2014-06-25 16:32:33 +0200

    pandoc script added. pandoc script to generate markdown and word documents from LaTeX source. also got rid of rtf format, but, if desired, a new line can be added to pandocit.sh

---
+++ manual/pandocit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This script calls pandoc to generate markdown and word documents
+# automatically. The created documents will need some minor editing.
+pandoc -f latex -t docx pymemdyn-manual.tex -o pymemdyn-manual.docx
+pandoc -f latex -t markdown pymemdyn-manual.tex -o pymemdyn-manual.md
