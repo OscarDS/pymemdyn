772f03d64605dbfef8d7bade0833e73c5386db17
Mauricio Esguerra <mauricio.esguerra@gmail.com>
2014-07-21 13:33:28 +0200

    fixed csb submission script.

--- scripts/run_csb.sh
+++ scripts/run_csb.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "#!/bin/bash -l
-source home/apps/gromacs-4.6.5/bin/GMXRC
+source /home/apps/gromacs-4.6.5/bin/GMXRC.bash
 source /home/apps/bin/apps.sh
-run.py -p a2b_filter.pdb
+run.py -p a2a_ag.pdb -l lig  --cho cho
 " > temp.sh
 chmod +x temp.sh
-sbatch -A snic2013-26-1 -n 8 --exclusive -t 48:00:00 -J pymemdyn temp.sh
+sbatch -n 8 -t 47:59:00 -J pymemdyn temp.sh
