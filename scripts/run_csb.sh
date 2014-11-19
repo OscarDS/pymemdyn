0be97720661c947e7587fec944676fa5d9b3c406
Mauricio Esguerra <mauricio.esguerra@gmail.com>
2014-11-19 13:12:16 +0100

    small typo in csb script.

--- scripts/run_csb.sh
+++ scripts/run_csb.sh
@@ -2,7 +2,7 @@
 echo "#!/bin/bash -l
 source /home/apps/gromacs-4.6.5/bin/GMXRC.bash
 source /home/apps/bin/apps.sh
-run.py -p a2a_ag.pdb -l lig  --cho cho
+pymemdyn -p a2a_ag.pdb --lig lig  --cho cho
 " > temp.sh
 chmod +x temp.sh
 sbatch -n 8 -t 47:59:00 -J pymemdyn temp.sh
