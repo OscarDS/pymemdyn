21f9094a230cbd21dc0d4de64dc508b0e4a6f0f2
Mauricio Esguerra <mauricio.esguerra@gmail.com>
2015-12-09 14:50:19 +0100

    updating clustersettings to clean non needed env vars.

--- clustersettings.py
+++ clustersettings.py
@@ -1,40 +1,10 @@
 import os
 
-#PID_FILE = "/home/gpcradmin/gpcr_modsim/run/PyroDaemon.pid"
-#LOG_FILE = "/home/gpcradmin/gpcr_modsim/log/PyroDaemon.log"
-#MODEL_LOG_FILE = "/home/gpcradmin/gpcr_modsim/log/model_file.log"
-#PYTHON_BIN = "/home/apps/bin/python2.7"
-#JAVA_PATH = "/usr/java/latest/bin/java"
 BINDIR = "/home/gpcradmin/gpcr_modsim/gpcr_backend/"
-#TEMPLATES_DIR = "/home/gpcradmin/gpcr_modsim/templates/"
-
-# Used in pyro_objects.py
-#SLURMOUT = "/home/gpcradmin/gpcr_modsim/slurm-out/"
-#PYMEMPATH = "/home/gpcradmin/gpcr_modsim/pymemdyn/"
-
-# STAMP related fields (used in stamp.py)
-#STAMP_BIN = "/home/apps/stamp.4.4/bin/linux/"
-#STAMP_DIR =  "/home/apps/stamp.4.4/defs/"
-#STAMP_TMPDIR = "/home/gpcradmin/tmp/"
-#CLUSTAL_BIN = "/home/apps/clustalw"
-#TEMPLATE_PDB = "/home/gpcradmin/gpcr_modsim/templates/3eml.pdb"
-
-# MOLPROBITY
-#MOLPROBITY_PATH = "/home/apps/molprobity/lib/"
-
-
-#SCRIPT_DIR = os.path.dirname(os.path.realpath(pymoldyn.__file__))
-
-# This is the dir where pymoldyn git repo has been deployed,
-# or to be more specific, this file settings.py is located
-#ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 #This dir now has to be the absolute path to the source of templates
 # You can refer it relatively like this:
 TEMPLATES_DIR = os.path.join(BINDIR, "templates")
-# Or absolutely like this:
-# TEMPLATES_DIR = "/path/to/your/templates"
-# But this WILL FAIL: * TEMPLATES_DIR = "templates" *
 
 # Choose a path to the gromacs binaries.
 #GROMACS_PATH = "/opt/applications/gromacs/4.0.5/gnu/ib/bin/"
@@ -67,5 +37,5 @@ QUEUE_NUM_NODES = 1
 QUEUE_NUM_PROCS = 8
 
 # Choose the maximum alloted time for your run.
-QUEUE_MAX_TIME = "47:59:00"
+QUEUE_MAX_TIME = "47:59:99"
 
