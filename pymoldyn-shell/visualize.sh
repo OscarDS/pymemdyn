51b56ebbf337e862c5d23debfba6c0a56396b832
Remco L. van den Broek <99324102+rlvandenbroek@users.noreply.github.com>
2022-04-07 22:05:20 +0200

    Delete pymoldyn-shell directory
    Obsolete

--- pymoldyn-shell/visualize.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# This command is often needed to get a file in the correct format for visualization 
-# in pymol.
-echo 1 ; echo 0 | trjconv -f traj.xtc -pbc mol -ur compact -s topol.tpr -o trajpbc.xtc -center
