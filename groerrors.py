35833b143db42b95e46c07bddcc415962809f8df
Rebecca Küppe <r.v.kupper@student.tue.nl>
2023-04-04 14:55:32 +0200

    Catch 'Fatal error' from GROMACS and print to log

--- groerrors.py
+++ groerrors.py
@@ -1,3 +1,6 @@
+import logging
+logger_GROerror = logging.getLogger('pymemdyn.groerrors')
+
 class GromacsError(BaseException):
     pass
 
@@ -25,6 +28,7 @@ class GromacsMessages(object):
          "Can not open file": IOGromacsError,
          # FIXME: Clearly the following is not IOError
          "srun: error: Unable to create job step": IOGromacsError,
+         "Fatal error:": IOGromacsError,         
          }
 
     def __init__(self, gro_err="", command="", *args, **kwargs):
@@ -32,6 +36,7 @@ class GromacsMessages(object):
         Pass the command and the output of that command in "command" and
         "gro_err" kwargs. The check for "error" property
         """
+        self.logger_groerrors = logging.getLogger('pymemdyn.groerrors.GromacsMessages')
         self.error = False
         self.command = command
         self.gro_err = gro_err.decode('utf-8').split("\n")
@@ -46,4 +51,10 @@ class GromacsMessages(object):
         for line in self.gro_err:
             for error in self.e.keys():
                 if line.startswith(error):
+                    if error == "Fatal error:":
+                        self.logger_groerrors.error("\n\nGROMACS FATAL ERROR:\n")
+                        j = self.gro_err.index('of the License, or (at your option) any later version.')
+                        self.logger_groerrors.error('\n'.join(self.gro_err[j+1:]))
+                        self.logger_groerrors.error("\n\n GROMACS FATAL ERROR\n")
+
                     raise self.e[error](self.command, error)
